43ba13ddf1e726924a63fde98645969c009e49d4
emanzuetaj <eliasmanz@gmail.com>
2019-04-22 22:41:38 -0400

    add heroku config

---
+++ build/precache-manifest.edf53f0442a21424763b7775d5a796bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15d906cb6c7c7df861f7",
+    "url": "/static/css/main.3b2d209a.chunk.css"
+  },
+  {
+    "revision": "15d906cb6c7c7df861f7",
+    "url": "/static/js/main.9f9c096a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "61f53f8c41951316ec6c",
+    "url": "/static/js/2.c45e3508.chunk.js"
+  },
+  {
+    "revision": "ad66af3c6d7ec3633728f5280985cb04",
+    "url": "/index.html"
+  }
+];
